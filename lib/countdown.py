51f621eb3ea55d14e59d85305db75f5b1c017b87
Madison <mlinkenmeyer@gmail.com>
2023-09-13 20:46:18 -0700

    countdown to midnight

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+from time import sleep
+
+def countdown(second):
+
+    while second:
+        print(f'{second} SECOND(S)!')
+        second -= 1
+
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(second):
+
+    while second:
+        print(f'{second} SECOND(S)!')
+        second -= 1
+        sleep(1)
+
+    print('HAPPY NEW YEAR!')
